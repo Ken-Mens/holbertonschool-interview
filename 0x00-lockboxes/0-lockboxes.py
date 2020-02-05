1fae2752e19d563c3ce6bd6a8fac95f7f87610b5
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-04 17:45:09 -0800

    program that checks whether or not lockboxes can be opened or not

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Algo checking wheter or not lockboxes can be opened"""
+
+def canUnlockAll(boxes):
+    """ Creates a set for non duplicates and list for found keys"""
+
+    k_opened = set()
+    k_found = [0]
+
+    while len(k_opened) < len(boxes):
+        if len(boxes) == 0:
+            return True
+        if not k_found or not isinstance(boxes, list):
+            return False
+        key = k_found.pop()
+        if key not in k_opened:
+            k_opened.add(key)
+            k_found.extend(boxes[key])
+    return True
