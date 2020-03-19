217b1c8d513ca30948fd6f7c3e8d147201b78a13
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-03-18 20:54:01 -0700

    fix pep8 formatting

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == '__main__':
 
-    def print_code(size, codes): 
+    def print_code(size, codes):
         """Printer function"""
         print("File size: {:d}".format(size))
         for idx in sorted(codes.keys()):
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     size = 0
     codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-         "403": 0, "404": 0, "405": 0, "500": 0}
+             "403": 0, "404": 0, "405": 0, "500": 0}
 
     count = 0
     try:
@@ -33,6 +33,7 @@ if __name__ == '__main__':
                 if count % 10 == 0:
                     print_code(size, codes)
             print_code(size, codes)
-    except KeyboardInterrupt: 
+    except KeyboardInterrupt:
         print_code(size, codes)
         raise
+    
