c7d910643bf5bc1adbd6199f955f7f6cb5c82a72
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-04-08 12:58:34 -0700

    utf8 validator

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+'''UTF-8 validator'''
+
+
+def validUTF8(data):
+        '''Checks if UTF-8 is valid'''
+        if not data:
+            return False
+        byte = 0
+        for p in data:
+            if byte>0:
+                if p & 192 == 128: byte -= 1
+                else: return False
+            elif p & 192 == 128: return False
+            elif p & 224 == 192: byte += 1
+            elif p & 240 == 224: byte += 2
+            elif p & 248 == 240: byte += 3
+            elif p > 248:        return False
+        return byte == 0
