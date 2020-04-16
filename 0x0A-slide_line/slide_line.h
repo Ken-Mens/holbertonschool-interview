17dd73e1d99aed021a834f6245353456dd46a778
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-04-15 21:54:11 -0700

    add fixed header file

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,8 +1,11 @@
 #ifndef SLIDE_LINE_H
 #define SLIDE_LINE_H
 
+#include <stdlib.h>
+
 #define SLIDE_LEFT -1
 #define SLIDE_RIGHT 1
+
 
 int slide_left(int *line, size_t size);
 int slide_right(int *line, size_t size);
