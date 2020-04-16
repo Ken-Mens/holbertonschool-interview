77b2f582e82ee3f8c509c9a6041da01da435a4e8
Ken-Mens <40308494+Ken-Mens@users.noreply.github.com>
2020-04-15 21:41:56 -0700

    Update slide header

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -4,8 +4,8 @@
 #define SLIDE_LEFT -1
 #define SLIDE_RIGHT 1
 
-int slidel(int *line, size_t size);
-int slider(int *line, size_t size);
+int slide_left(int *line, size_t size);
+int slide_right(int *line, size_t size);
 int slide_line(int *line, size_t size, int direction);
 
 #endif /*SLIDE_LINE_H*/
