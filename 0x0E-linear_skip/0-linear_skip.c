29f0038b32a9acd1de9b93ba65adf9f163cec5b0
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-05-27 18:33:44 -0700

    changed while loop in function

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -9,45 +9,43 @@
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *skipd = NULL;
-	skiplist_t *plchd = NULL;
+    skiplist_t *skipd = NULL;
+    skiplist_t *plchd = NULL;
 
-	if (list == NULL)
-		return (NULL);
-	skipd = list;
-	while (skipd)
-	{
-		plchd = skipd->express;
-		printf("Value checked at index [%lu] = [%i]\n",
-		       (plchd)->index, (plchd)->n);
-		if ((plchd)->n >= value)
-		{
-			printf("Value found between indexes [%lu] and [%lu]\n",
-			       skipd->index, (plchd)->index);
-			break;
-		}
-		if (!(plchd)->express)
-		{
-			skipd = skipd->express;
-			plchd = skipd;
-			while (plchd->next)
-				plchd = plchd->next;
-			printf("Value found between indexes [%lu] and [%lu]\n",
-			       skipd->index, plchd->index);
-			break;
-		}
-		skipd = skipd->express;
-	}
-	while (plchd <= skipd->next)
-	{
-		printf("Value checked at index [%lu] = [%i]\n",
-		       skipd->index, skipd->n);
-		if (skipd->n == value)
-			return (skipd);
-		if (skipd->n > value)
-			return (NULL);
-		skipd = skipd->next;
-	}
-	printf("Value checked at index [%lu] = [%i]\n", skipd->index, skipd->n);
-	return (NULL);
+    if (list == NULL)
+        return (NULL);
+    skipd = list;
+    while (skipd)
+    {
+        plchd = skipd->express;
+        printf("Value checked at index [%lu] = [%i]\n",
+               (plchd)->index, (plchd)->n);
+        if ((plchd)->n >= value)
+        {
+            break;
+        }
+        if (!(plchd)->express)
+        {
+            skipd = skipd->express;
+            plchd = skipd;
+            while (plchd->next)
+                plchd = plchd->next;
+            break;
+        }
+        skipd = skipd->express;
+    }
+	printf("Value found between indexes [%lu] and [%lu]\n",
+    		skipd->index, (plchd)->index);
+    while (plchd <= skipd->next)
+    {
+        printf("Value checked at index [%lu] = [%i]\n",
+               skipd->index, skipd->n);
+        if (skipd->n == value)
+            return (skipd);
+        if (skipd->n > value)
+            return (NULL);
+        skipd = skipd->next;
+    }
+    printf("Value checked at index [%lu] = [%i]\n", skipd->index, skipd->n);
+    return (NULL);
 }
