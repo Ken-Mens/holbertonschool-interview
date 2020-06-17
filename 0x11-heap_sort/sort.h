66f9d0524747f14139eb43fd02f2ca7f88cd6747
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-06-17 16:31:31 -0700

    sorting algo header

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef HEAP_SORT_H
+#define HEAP_SORT_H
+#include <stdlib.h>
+#include <stdio.h>
+
+void swap(int *beta, int *zeta);
+void heapify(int *array, size_t n, int index, size_t size);
+void heap_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+#endif /*HEAP_SORT_H*/
