5e0519710a879865250cd6cc1ed68e13d9f7ab32
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-06-24 14:34:37 -0700

    renamed methods

--- 0x12-advanced_binary_search/search_algos.h
+++ 0x12-advanced_binary_search/search_algos.h
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void print_away(int *array, size_t left, size_t right);
-int binary_search(int *array, size_t left, size_t right, int value);
+void print_away(int *array, size_t l, size_t r);
+int binary_search(int *array, size_t l, size_t r, int value);
 int advanced_binary(int *array, size_t size, int value);
 
 
