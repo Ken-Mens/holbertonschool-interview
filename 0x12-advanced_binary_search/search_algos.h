a46684b96e92f52fa0f8cc25624b5436494d77ba
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-06-24 14:32:17 -0700

    header file for search algo task

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef ADV_BIN_H
+#define ADV_BIN_H
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_away(int *array, size_t left, size_t right);
+int binary_search(int *array, size_t left, size_t right, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+
+#endif /*ADV_BIN_H*/
