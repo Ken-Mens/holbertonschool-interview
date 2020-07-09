3f9fb28108acc98c0427c908b758286f0a0944b1
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-07-08 23:20:35 -0700

    fix header file

--- 0x14-heap_extract/binary_trees.h
+++ 0x14-heap_extract/binary_trees.h
@@ -12,16 +12,17 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *tree);
 int heap_extract(heap_t **root);
 void heapify(heap_t *heap);
 void swap(binary_tree_t *node1, binary_tree_t *node2);
