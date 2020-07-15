7cf272a16b2c25047d9f1b791967f40215d018a1
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-07-15 16:22:49 -0700

    add holberton header file

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdlib.h>
+#include <stdio.h>
+int _putchar(char c);
+int _strlen(char *str);
+void *mem_set(void *q, int c, int size);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif /* HOLBERTON_H*/
