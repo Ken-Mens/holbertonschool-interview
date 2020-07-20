2f35e6a9a57a674e532cfe295083fc4705ded513
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-07-20 10:50:41 -0700

    add variable line to function

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,7 +9,8 @@ def rotate_2d_matrix(matrix):
     rotate matrix in place by 90 degrees
     """
     n = len(matrix)
-    for y in range(0, int(n/2)):
+    fooey = int(n/2)
+    for y in range(0, fooey):
         for idx in range(y, n-y-1):
             foo = matrix[y][idx]
             matrix[y][idx] = matrix[n-idx-1][y]
