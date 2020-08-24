45fa7bc1fdcff9719b90ab3d64fbb3b2a3bbf468
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-08-24 16:07:53 -0700

    add making change algo

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Given a pile of coins of different values, determine the fewest number of coins
+needed to match a certain amount.
+"""
+
+
+def makeChange(coins, total):
+    """
+    change making function
+    """
+    if (total == 0):
+        return 0
+    coins.sort()
+    coins.reverse()
+    change = 0
+    for c in coins:
+        if c <= 0:
+            pass
+        foo = (total // c)
+        change += foo
+        total -= (foo * c)
+    if total != 0:
+        return (-1)
+    return change
