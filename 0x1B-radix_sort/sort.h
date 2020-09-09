d368bf93390714327d20b10e780531c5a3025b2e
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-09-09 12:49:46 -0700

    add sort header file

---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,10 @@
+#ifndef SORT_HEAD
+#define SORT_HEAD
+#include <stdlib.h>
+
+void print_array(const int *array, size_t size);
+int max_int(int *array, int bulk);
+void count_sort(int *array, size_t size, int digit);
+void radix_sort(int *array, size_t size);
+
+#endif /*SORT_HEAD*/
