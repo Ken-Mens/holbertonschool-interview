e7349c6b735b44330f3dc7a75dcdf6b1e87a41f2
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-09-09 12:50:05 -0700

    add radix sort function

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,78 @@
+#include "sort.h"
+
+/**
+ * max_int - maximum integer in an unsorted int array
+ * @array: array to be iterated
+ * @sbulk: size of the array
+ * Return: max int in the array
+ */
+
+int max_int(int *array, int bulk)
+{
+	int mx = array[0];
+	int foo;
+
+	foo = 1;
+	for (; foo < bulk; foo++)
+	{
+		if (array[foo] > mx)
+			mx = array[foo];
+	}
+	return (mx);
+}
+
+/**
+ * count_sort - Counting sort for Radix sort
+ * @array: array to sort
+ * @size: size of array
+ * @n: sig fig to sort by
+ * Return: void
+ */
+
+void count_sort(int *array, size_t size, int n)
+{
+	int *output = NULL;
+	int count[10] = {0};
+	size_t idx = 0;
+
+	output = malloc(sizeof(int) * size);
+	for (; idx < size; idx++)
+		count[(array[idx] / n) % 10]++;
+	for (idx = 1; idx < 10; idx++)
+		count[idx] += count[idx - 1];
+	idx = size - 1;
+	while (1)
+	{
+		output[count[(array[idx] / n) % 10] - 1] = array[idx];
+		count[(array[idx] / n) % 10]--;
+		if (idx == 0)
+			break;
+		idx--;
+	}
+	idx = 0;
+	for (; idx < size; idx++)
+		array[idx] = output[idx];
+	free(output);
+}
+
+/**
+ * radix_sort - radix sort
+ * @array: array to sort
+ * @size: size of array to sort
+ * Return: void
+ */
+
+void radix_sort(int *array, size_t size)
+{
+	size_t mx, n;
+
+	if (!array)
+		return;
+	mx = max_int(array, size);
+	n = 1;
+	for (; mx / n > 0; n *= 10)
+	{
+		count_sort(array, size, n);
+		print_array(array, size);
+	}
+}
