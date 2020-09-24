c0ad5f96ef4e7dd5e029d133beb0f28b66ff8fda
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-09-23 20:18:56 -0700

    add method avl tree

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -11,43 +11,43 @@ int binary_tree_is_avl(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (bst_check(tree, INT_MIN, INT_MAX) != 1)
+	if (best_check(tree, INT_MIN, INT_MAX) != 1)
 		return (0);
 	return (1);
 }
 
 /**
- * bst_check - checks for bestness
+ * best_check - checks for bestness
  * @root: root node of tree
  * @min: minimum value constraint
  * @max: maximum value constraint
  * Return: 1 if bst, 0 otherwise
  */
 
-int bst_check(const binary_tree_t *root, int min, int max)
+int best_check(const binary_tree_t *root, int min, int max)
 {
 	if (root == NULL)
 		return (1);
 	if (root->n < min || root->n > max)
 		return (0);
-	return (bst_check(root->left, min, root->n - 1) &&
-		bst_check(root->right, root->n + 1, max));
+	return (best_check(root->left, min, root->n - 1) &&
+		best_check(root->right, root->n + 1, max));
 }
 
 /**
  * height_check - check tree height
- * @root: root node of tree
- * Return: height of tree
+ * @root: root node of  avl tree
+ * Return: height of  avl tree
  */
 
-int height_check(const binary_tree_t *root)
+int height_check(const binary_tree_t *tree)
 {
 	int l, r;
 
-	if (root == NULL)
+	if (tree == NULL)
 		return (0);
-	l = height_check(root->left);
-	r = height_check(root->right);
+	l = height_check(tree->left);
+	r = height_check(tree->right);
 	if (r > l)
 		return (1 + r);
 	return (1 + l);
