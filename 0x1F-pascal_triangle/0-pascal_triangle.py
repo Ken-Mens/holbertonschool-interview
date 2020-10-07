540ea65441d651435fec91839cf34617b69d4f07
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-10-07 11:13:54 -0700

    add updated pascale triangle function

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -2,14 +2,14 @@
 """ return list of integers that represent Pascale's triangle"""
 
 def pascal_triangle(n):
-    a = [[] for idx in range(n)]
+    a = [[] for idx in range(0, n)]
     for idx in range(0, n):
-        for j in range(idx + 1):
-            if (j < idx):
-                if (j == 0):
+        for foo in range(idx + 1):
+            if (foo < idx):
+                if (foo == 0):
                     a[idx].append(1)
                 else:
-                    a[idx].append(a[idx - 1][j] + a [idx -1][j -1])
-            elif (j == idx): 
+                    a[idx].append(a[idx - 1][foo] + a [idx -1][foo - 1])
+            elif (foo == idx): 
                 a[idx].append(1)
     return a
