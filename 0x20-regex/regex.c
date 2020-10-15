f426768c6ffec83275364dfc259ae3c571f139b0
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-10-14 19:04:53 -0700

    add regex matching function

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,28 @@
+#include "regex.h"
+
+/**
+* regex_match - matches patterns with string
+* @str: string
+* @pattern: pattern which contains alphabetic chars, wildcard, and '.'
+* Return: 1 if true, 0 if false
+*/
+int regex_match(char const *str, char const *pattern)
+{
+	int zilch = 0;
+
+	if (!str)
+		return (zilch);
+	if (!*str)
+	{
+		if (*(pattern + 1) == '*')
+			return (regex_match(str, pattern + 2));
+		return (!*pattern);
+	}
+	if (*(pattern + 1) == '*')
+	{
+		if (*pattern == '.' || *pattern == *str)
+			return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
+		return (regex_match(str, pattern + 2));
+	}
+	return (zilch);
+}
