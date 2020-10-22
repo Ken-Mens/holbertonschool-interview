ead4302ebca964bbc9f3396c698667d8d7abea25
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-10-21 22:14:47 -0700

    add substring function

--- 0x21-substring/substring.c
+++ 0x21-substring/substring.c
@@ -1,49 +1,56 @@
 #include "substring.h"
 
 /**
- * find_substring - finds possible substrings
+ * find_substring - find all possible substrings
  * @s: is the string to scan
- * @words: array of substring must be concatenated
- * @nb_words: number of elements in array words
- * @n: holds address to store number of elements
+ * @words: array of substring fragments
+ * @nb_words: is the number of elements in the array words
+ * @n: holds the address at which to store the number
  * Return: array containing indexes
  */
 
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-    char *temp = NULL, *p = NULL;
-    int *p_mark, count = 0, regwordlen = strlen(words[0]), s_len = strlen(s);
-    int  dict_len = 0;
-    dict_len =regwordlen*nb_words;
-    temp = calloc((dict_len+1),sizeof(char));       
-    p_mark = calloc((s_len+1),sizeof(int));
-    for(int i = 0; i <= (s_len-dict_len);){
-        strncpy(temp, &s[i],dict_len);
-        for(int j = 0; j< nb_words; j++)
-        {
-            for(int k = 0; k<dict_len ;)
-            {
-                p = strstr(temp+k, words[j]);
-                if(p!= NULL && ((p-temp) % regwordlen)==0)
-                    break;
-                else if(NULL!=p){
-                    k = p-temp ++;
-                    p = NULL;
-                }else if(NULL == p)
-                    goto foo;       
-            }          
-            if(NULL != p )
-                memset(p, '.', regwordlen);
-            else
-                goto foo;           
-        }
-                
-        if(s_len!=0 && dict_len!=0)
-            p_mark[count++]=i;
-foo:
-        i++;
-    }  
-    *n = count;
+	char *temp_o, *foo;
+	int slen, dict_len = 0, count = 0, regwordlen, l, m, k;
+	int *pos_m = NULL;
 
-    return p_mark;
+	slen = strlen(s);
+	regwordlen = strlen(words[0]);
+	dict_len = regwordlen * nb_words;
+	temp_o = calloc((dict_len + 1), sizeof(char));
+	pos_m = calloc((slen + 1), sizeof(char));
+    l = 0;
+	for (; l <= (slen - dict_len);)
+	{
+		strncpy(temp_o, &s[l], dict_len);
+        m = 0;
+		for (; m < nb_words; m++)
+		{
+            k = 0;
+			for (; k < dict_len;)
+			{
+				foo = strstr(temp_o + k, words[m]);
+				if (foo != NULL && ((foo - temp_o) % regwordlen) == 0)
+					break;
+				else if (foo != NULL)
+				{
+					k = foo - temp_o + 1;
+					foo = NULL;
+				}
+				else if (foo == NULL)
+					goto last;
+			}
+			if (foo != NULL)
+				memset(foo, '.', regwordlen);
+			else
+				goto last;
+		}
+		if (slen != 0 && dict_len != 0)
+			pos_m[count++] = l;
+last:
+		l++;
+	}
+	*n = count;
+	return (pos_m);
 }
