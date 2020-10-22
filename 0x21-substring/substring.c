f2db4966b257735b12c170a72c2916315a4ec6b3
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-10-21 19:14:50 -0700

    add updated function for finding substrings

--- 0x21-substring/substring.c
+++ 0x21-substring/substring.c
@@ -4,46 +4,41 @@
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
     char *temp;
-    int slen = strlen(s);
-    int *pmark;
-    int dictlen=0, count=0;
+    int s_len = strlen(s);
+    int *p_mark, dictlen = 0, count = 0, regwordlen = strlen(words[0]);
     char *p=NULL;
-    int regwordlen = strlen(words[0]);
     dictlen=regwordlen*nb_words;
     temp = calloc((dictlen+1),sizeof(char));
         
-    pmark = calloc((slen+1),sizeof(int));
-    int i=0;
-    for(; i<= (slen-dictlen);){
+    p_mark = calloc((s_len+1),sizeof(int));
+    for(int i = 0; i<= (s_len-dictlen);){
         strncpy(temp, &s[i],dictlen);
-        for(int j=0; j< nb_words; j++){
-     
-            for(int k=0; k<dictlen ;){
-                p=strstr(temp+k, words[j]);
+        for(int j = 0; j< nb_words; j++){
+
+            for(int k = 0; k<dictlen ;)
+            {
+                p = strstr(temp+k, words[j]);
                 if(p!= NULL && ((p-temp) % regwordlen)==0)
                     break;
                 else if(NULL!=p){
-                    k=p-temp+1;
-                    p=NULL;
-                }else if(NULL==p)
-                    goto nextseg;
-                
+                    k = p-temp ++;
+                    p = NULL;
+                }else if(NULL == p)
+                    goto nextseg;       
             }
             
             if(NULL!=p )
                 memset(p, '.', regwordlen);
             else
-                goto nextseg;
-            
+                goto nextseg;           
         }
                 
-        if(slen!=0 && dictlen!=0)
-            pmark[count++]=i;
+        if(s_len!=0 && dictlen!=0)
+            p_mark[count++]=i;
 nextseg:
         i++;
-    }
-   
+    }  
     *n = count;
-    
-    return pmark;
+
+    return p_mark;
 }
