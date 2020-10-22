afcd740d45b46df5c69c8bd8e2bcaed97641fae3
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-10-21 19:21:33 -0700

    add simple regex if condition

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -24,7 +24,7 @@ int regex_match(char const *str, char const *pattern)
 			return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
 		return (regex_match(str, pattern + 2));
 	}
-	if (*pattern == '.')
+	if (*pattern == '.' || *pattern == *str)
 	{
 		return (regex_match(str + 1, pattern + 1));
 	}
